a2d1e4fb0a7b4a739d3244289ce198bb7f506dad
Saulius Gražulis <grazulis@ibt.lt>
2025-12-09 19:02:42 +0200

    Getting file name from the command line.

--- nw_align.adb
+++ nw_align.adb
@@ -1,10 +1,15 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Ada.Command_Line; use Ada.Command_Line;
 
 with FASTA; use FASTA;
 
 procedure NW_Align is
    
 begin
-   Put_Line (To_String (Load_FASTA ("seq1.fasta")));
+   
+   if Argument_Count >= 1 then
+      Put_Line (To_String (Load_FASTA (Argument (1))));
+   end if;
+   
 end;
