e7f4db067682a8db8e6896747323e828611eea85
Saulius Gražulis <grazulis@ibt.lt>
2025-12-09 18:45:20 +0200

    Implementing FASTA file input.

--- nw_align.adb
+++ nw_align.adb
@@ -5,9 +5,17 @@ procedure NW_Align is
    
    function Load_FASTA (Input : File_Type) return Unbounded_String is
       Header : String := Get_Line (Input);
+      Result : Unbounded_String := Null_Unbounded_String;
    begin
-      Put_Line ("Header: " & Header);
-      return To_Unbounded_String (Header);
+      while not End_Of_File (Input) loop
+         declare
+            Line : String := Get_Line (Input);
+         begin
+            exit when Line (1) = '>';
+            Append (Result, Line);
+         end;
+      end loop;
+      return Result;
    end;
    
    function Load_FASTA (File_Name : String) return Unbounded_String is
