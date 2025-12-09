74a030bbde75f8aeb7103777a31063c2181e7878
Saulius Gražulis <grazulis@ibt.lt>
2025-12-09 19:19:00 +0200

    Adding specification of the procedure "Align".

--- nw_align.adb
+++ nw_align.adb
@@ -6,6 +6,25 @@ with FASTA; use FASTA;
 
 procedure NW_Align is
    
+   Default_Match_Score        : constant Integer := 1;
+   Default_Mismatch_Penalty   : constant Integer := -1;
+   Default_Gap_Open_Penalty   : constant Integer := -4;
+   Default_Gap_Extend_Penalty : constant Integer := -2;
+   
+   procedure Align
+     (
+      Seq1, Seq2 : in Unbounded_String;
+      Aln1, Aln2 : out Unbounded_String;
+      Score      : out Long_Integer;
+      Match_Score        : Integer := Default_Match_Score;
+      Mismatch_Penalty   : Integer := Default_Mismatch_Penalty;
+      Gap_Open_Penalty   : Integer := Default_Gap_Open_Penalty; 
+      Gap_Extend_Penalty : Integer := Default_Gap_Extend_Penalty
+     ) is
+   begin
+      null;
+   end;
+   
 begin
    
    if Argument_Count >= 1 then
